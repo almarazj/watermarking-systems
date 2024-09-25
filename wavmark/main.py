5a268f2f24bf5492d37dbae5b8f86d6108c5d9cf
almarazj <almaraz43493@estudiantes.untref.edu.ar>
2024-09-25 20:14:35 -0300

    Added training loop

--- wavmark/main.py
+++ wavmark/main.py
@@ -1,46 +1,115 @@
 import numpy as np
 import torch
-import wavmark
-import soundfile as sf
-from wavmark.utils import file_reader
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from utils.custom_loss import WatermarkLoss
+from models.discriminator import Discriminator
+from models.my_model import Model
+import json
+from utils.get_dataloader import get_loader
 
-# 1.load model
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-print(f"Device: {device}")
-model = wavmark.load_model().to(device)
-nb_params = sum(param.numel() for param in model.parameters())
-print(f"Number of model parameters: {nb_params}")
+def train_epoch(
+    trn_loader: DataLoader,
+    model,
+    discriminator,
+    model_optimizer: torch.optim.Adam,
+    disc_optimizer: torch.optim.Adam,
+    device: torch.device):
 
-# 2.create 16-bit payload
-payload = np.random.choice([0, 1], size=16)
-print("Payload:", payload)
+    running_loss = 0
+    num_total = 0.0
+    model.train()
+    discriminator.train()
+    criterion = WatermarkLoss()
+    
+    for batch_x, batch_m in trn_loader:
+        print(f"Batch {num_total}")
+        batch_size = batch_x.size(0)
+        num_total += batch_size
+        
+        batch_x = batch_x.to(device)
+        batch_m = batch_m.to(device)
+        
+        # Encode
+        batch_x_out = model.encode(batch_x, batch_m)
+        batch_x_out = batch_x_out.to(device)
+        # Decode
+        batch_m_out = (model.decode(batch_x_out) >= 0.5).float().detach()
+        batch_m_out = batch_m_out.to(device)
+        
+        # Discriminator
+        real_preds = discriminator(batch_x.unsqueeze(1))
+        fake_preds = discriminator(batch_x_out.unsqueeze(1))
+        
+        # Update model (generator)
+        model_optimizer.zero_grad()
+        disc_optimizer.zero_grad()
+        
+        # Compute loss
+        total_loss  = criterion(batch_m, batch_x, batch_m_out, batch_x_out, real_preds, fake_preds)
+        total_loss.backward()
 
-signal = file_reader.read_as_single_channel("audio-files\\test.wav", aim_sr=16000)
+        model_optimizer.step()
+        disc_optimizer.step()        
+   
+        running_loss += total_loss.item() * batch_size
+        print(f"Running loss: {running_loss}")
+    running_loss /= num_total
+    return running_loss
 
-watermarked_signal, _ = wavmark.encode_watermark(model, signal, payload, show_progress=True)
-payload_decoded, _ = wavmark.decode_watermark(model, watermarked_signal, show_progress=True)
-BER = (payload != payload_decoded).mean() * 100
-print("Decode BER:%.1f" % BER)
-
-sf.write('audio-files\\wavmark_wm_audio.wav', watermarked_signal, 16000)
-
-
-# Ejemplo de uso:
-# Suponiendo que tienes implementados `encoder`, `decoder` y `discriminator`
-encoder = ...  # Encoder model
-decoder = ...  # Decoder model
-discriminator = ...  # Discriminator model
-
-# Datos de ejemplo
-mvec = torch.randn(1, 32)               # Vector del mensaje
-x_wave = torch.randn(1, 1, 16000)       # Audio original
-x_wave_prime = torch.randn(1, 1, 16000) # Audio con marca de agua
-z = torch.randn(1, 10)                  # Vector auxiliar
-
-# Crear la función de pérdida
-loss_fn = WatermarkLoss(lambda_a=0.1, lambda_g=0.1)
-
-# Calcular las pérdidas
-Ltotal, Ld = loss_fn(mvec, x_wave, x_wave_prime, z, discriminator, encoder, decoder)
-
-print(f"Ltotal: {Ltotal.item()}, Ld: {Ld.item()}")
+def train(loaders,
+          model,
+          discriminator,
+          device,
+          config):
+    print("Start training...")
+    train_losses = []
+    model_optimizer = optim.Adam(model.parameters(), lr=config["LEARNING_RATE"])
+    disc_optimizer = optim.Adam(discriminator.parameters(), lr=config["LEARNING_RATE"])
+    for epoch in range(config["NUM_EPOCHS"]):
+        print(f"Epoch: {epoch}")
+        train_loss = train_epoch(loaders["train"], model, discriminator, model_optimizer, disc_optimizer, device)
+        train_losses.append(train_loss)
+        
+        
+def get_data_paths(conf_path: str) -> dict:
+    # Read json file
+    with open(conf_path, "r") as f_json:
+        paths = json.loads(f_json.read())
+    return paths
+    
+def main():
+    
+    config = {
+        "BATCH_SIZE": 4,
+        "LEARNING_RATE": 1e-4,
+        "NUM_EPOCHS": 50, 
+        "SEED": 1234,
+        "NUM_POINTS": 16000,
+        "NUM_BITS": 16,
+        "FFT_SIZE": 1000,
+        "HOP_LENGTH": 400,
+        "NUM_LAYERS": 8
+        }
+    
+    conf_path = "./datasets/paths.conf"
+    data_paths = get_data_paths(conf_path)
+    
+    # 1.load model
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    print(f"Device: {device}")
+    
+    discriminator = Discriminator().to(device)
+    
+    model = Model(num_point=config["NUM_POINTS"],
+                  num_bit=config["NUM_BITS"],
+                  n_fft=config["FFT_SIZE"],
+                  hop_length=config["HOP_LENGTH"],
+                  num_layers=config["NUM_LAYERS"]).to(device)
+    
+    loaders = get_loader(config["SEED"], data_paths, config["BATCH_SIZE"])
+    
+    train(loaders, model, discriminator, device, config)
+    
+if __name__ == '__main__':   
+    main()
